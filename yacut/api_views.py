063938ed7ecddcf6428598d661be8e39bbb5e58b
Yaroslav Bochkov <cruelty86@yandex.ru>
2024-12-19 19:21:36 +0300

    test protocol added

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,5 +1,6 @@
 # yacut/api_views.py
 from http import HTTPStatus
+import re
 
 from flask import jsonify, request, url_for
 from sqlalchemy.exc import IntegrityError
@@ -7,6 +8,7 @@ from sqlalchemy.exc import IntegrityError
 from yacut import app
 from yacut.error_handlers import (
     InvalidAPIUsage,
+    URLValidationError,
     validate_url_map
 )
 from yacut.models import URLMap
@@ -19,6 +21,11 @@ ID_NOT_FOUND = 'Указанный id не найден'
 # Константа для имени view-функции
 REDIRECT_ENDPOINT = 'redirect_short_url'
 
+# Регулярное выражение для проверки URL
+URL_PATTERN = re.compile(
+    r'^https?://(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$'
+)
+
 
 @app.route('/api/id/', methods=['POST'])
 def generate_short_url():
@@ -27,13 +34,27 @@ def generate_short_url():
         raise InvalidAPIUsage(INCORRECT_REQUEST)
 
     data = request.get_json(silent=True)
-    validated_data = validate_url_map(data)
+
+    # Валидация данных с проверкой протокола
+    if not data:
+        raise URLValidationError('Отсутствует тело запроса')
+
+    url = data.get('url')
+    if not url:
+        raise URLValidationError('"url" является обязательным полем!')
+
+    # Проверка протокола
+    if not URL_PATTERN.match(url):
+        raise URLValidationError('Указан некорректный URL')
 
     try:
+        validated_data = validate_url_map(data)
         url_map = URLMap.create(
             original=validated_data['url'],
             short=validated_data.get('custom_id')
         )
+    except URLValidationError as e:
+        return jsonify({'message': e.message}), HTTPStatus.BAD_REQUEST
     except IntegrityError:
         raise InvalidAPIUsage(LINK_ALREADY_EXISTS)
 
