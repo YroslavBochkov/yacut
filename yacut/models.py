374c04b7b93a7a7a9526a8d40971b7bdab6cfe2f
Yaroslav Bochkov <cruelty86@yandex.ru>
2024-12-22 15:11:14 +0300

    models rev

--- yacut/models.py
+++ yacut/models.py
@@ -1,5 +1,4 @@
 import random
-import re
 from datetime import datetime
 
 from sqlalchemy.exc import IntegrityError
@@ -49,60 +48,54 @@ class URLMap(db.Model):
         )
 
     @staticmethod
+    def _get_or_raise(short, error_message):
+        """Получает объект или вызывает исключение."""
+        result = URLMap.query.filter_by(short=short).first()
+        if result is None:
+            raise URLMap.URLValidationError(error_message)
+        return result
+
+    @staticmethod
     def get_unique_short_id():
         """Метод создает уникальную короткую ссылку."""
         for _ in range(Config.MAX_UNIQUE_ID_ATTEMPTS):
             short_url = ''.join(
                 random.choices(
-                    population=SHORT_URL_CHARS, 
+                    population=SHORT_URL_CHARS,
                     k=URLMap.GENERATED_SHORT_ID_LENGTH
                 )
             )
             if URLMap.query.filter_by(short=short_url).first() is None:
                 return short_url
-        raise URLMap.URLValidationError(
-            URLMap.ERROR_GENERATION_FAILED
-        )
+        raise RuntimeError(URLMap.ERROR_GENERATION_FAILED)
 
-    @classmethod
-    def get_by_short(cls, short):
+    @staticmethod
+    def get_by_short(short):
         """Метод получает объект по его короткой ссылке."""
-        obj = cls.query.filter_by(short=short).first()
-        if obj is None:
-            raise cls.URLValidationError(cls.ERROR_NOT_FOUND)
-        return obj
+        return URLMap._get_or_raise(short, URLMap.ERROR_NOT_FOUND)
 
-    @classmethod
-    def create(cls, original, short=None):
+    @staticmethod
+    def create(original, short=None):
         """Создание новой записи с проверкой и генерацией короткой ссылки."""
-        if len(original) > MAX_LEN_ORIGINAL:
-            raise cls.URLValidationError(cls.ERROR_LONG_URL)
-
         if short:
             if len(short) > MAX_LEN_SHORT:
-                raise cls.URLValidationError(
-                    cls.ERROR_INVALID_SHORT_URL
+                raise URLMap.URLValidationError(
+                    URLMap.ERROR_INVALID_SHORT_URL
                 )
-            if not re.match(Config.SHORT_URL_PATTERN, short):
-                raise cls.URLValidationError(
-                    cls.ERROR_INVALID_SHORT_URL
-                )
-            if cls.query.filter_by(short=short).first():
-                raise cls.URLValidationError(
-                    cls.ERROR_DUPLICATE_SHORT_URL
+            if not all(char.isascii() and char.isalnum() for char in short):
+                raise URLMap.URLValidationError(
+                    URLMap.ERROR_INVALID_SHORT_URL
                 )
 
         if not short:
-            short = cls.get_unique_short_id()
+            short = URLMap.get_unique_short_id()
 
-        url_map = cls(original=original, short=short)
-
-        try:
-            db.session.add(url_map)
-            db.session.commit()
-            return url_map
-        except IntegrityError:
-            db.session.rollback()
-            raise cls.URLValidationError(
-                cls.ERROR_DUPLICATE_SHORT_URL
+        if URLMap.query.filter_by(short=short).first():
+            raise URLMap.URLValidationError(
+                URLMap.ERROR_DUPLICATE_SHORT_URL
             )
+
+        url_map = URLMap(original=original, short=short)
+        db.session.add(url_map)
+        db.session.commit()
+        return url_map
